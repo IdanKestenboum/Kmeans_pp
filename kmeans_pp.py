6a40ddfbc8f10073358f33a92bc6463c8e8a99e6
Idan <idankes1@gmail.com>
2022-05-02 23:28:44 +0300

    idan5

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -8,8 +8,9 @@ np.random.seed(0)
 input1=pd.read_csv('input_1_db_1.txt',delimiter=',',header=None)
 input2=pd.read_csv('input_1_db_2.txt',delimiter=',',header=None)
 inner=pd.DataFrame.merge(input1,input2,how='inner',left_on=0,right_on=0)
+inner.set_index(inner[0],inplace=True)
 inner=inner.iloc[:,1:]
-inner_array=inner.to_numpy() 
+inner_array=inner.to_numpy()
 N=inner_array.shape[0]
 K=3
 
@@ -26,16 +27,17 @@ i=1
 centroid_list=[None]*K
 D=[0]*N
 P=[0]*N
-centorid1=inner_array[np.random.choice(N)]
+centorid1=inner.loc[np.random.choice(N)]
 centroid_list[0]=centorid1
 while i<K:
     for l in range(N):
-        D[l]=find_argmin_cluster_to_point(centroid_list,inner_array[l],i)
+        D[l]=find_argmin_cluster_to_point(centroid_list,inner.loc[l],i)
     for l in range(N):
         P[l]=D[l]/np.sum(D)
     i+=1
-    centroid_list[i-1]=inner_array[np.random.choice(N,p=P)]
+    centroid_list[i-1]=inner.loc[np.random.choice(N,p=P)]
 for centroid in centroid_list:
-    index=np.where(inner_array==centroid)
-    print(index[0][0])
+    print(centroid.name)
+
+
 
